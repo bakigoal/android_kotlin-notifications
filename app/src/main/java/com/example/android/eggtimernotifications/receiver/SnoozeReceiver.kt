ae8d8ea599e63f5add9c4a357cc2b176b113f935
Ilmir Bakirov <ilmirbakirov@gmail.com>
2022-01-10 12:51:07 +0500

    add snooze action

--- app/src/main/java/com/example/android/eggtimernotifications/receiver/SnoozeReceiver.kt
+++ app/src/main/java/com/example/android/eggtimernotifications/receiver/SnoozeReceiver.kt
@@ -6,10 +6,15 @@ import android.content.Intent
 import android.os.SystemClock
 import android.text.format.DateUtils
 import com.example.android.eggtimernotifications.service.AlarmService
+import com.example.android.eggtimernotifications.util.notificationManager
 
 class SnoozeReceiver : BroadcastReceiver() {
 
     override fun onReceive(ctx: Context, intent: Intent) {
+        // cancel current notification
+        ctx.notificationManager().cancelAll()
+
+        // set new trigger time
         val triggerTime = SystemClock.elapsedRealtime() + DateUtils.MINUTE_IN_MILLIS
         AlarmService(ctx).setAlarm(triggerTime)
     }
--- app/src/main/java/com/example/android/eggtimernotifications/util/NotificationUtils.kt
+++ app/src/main/java/com/example/android/eggtimernotifications/util/NotificationUtils.kt
@@ -5,6 +5,7 @@ import android.app.NotificationManager
 import android.app.PendingIntent
 import android.content.Context
 import android.content.Intent
+import android.graphics.Bitmap
 import android.graphics.BitmapFactory
 import androidx.core.app.NotificationCompat
 import androidx.core.content.ContextCompat
@@ -20,70 +21,70 @@ private val FLAGS = 0
 fun Context.notificationManager() =
     ContextCompat.getSystemService(this, NotificationManager::class.java) as NotificationManager
 
-// TODO: Step 1.1 extension function to send messages (GIVEN)
 /**
  * Builds and delivers the notification.
- *
  * @param context, activity context.
  */
-@SuppressLint("UnspecifiedImmutableFlag")
 fun NotificationManager.sendNotification(messageBody: String, applicationContext: Context) {
-    // Create the content intent for the notification, which launches
-    // this activity
-    // TODO: Step 1.11 create intent
-    val contentIntent = Intent(applicationContext, MainActivity::class.java)
-
-    // TODO: Step 1.12 create PendingIntent
-    val contentPendingIntent = PendingIntent.getActivity(
-        applicationContext,
-        NOTIFICATION_ID,
-        contentIntent,
-        PendingIntent.FLAG_UPDATE_CURRENT
-    )
-
-    // TODO: Step 2.0 add style
     val eggImage = getEggImage(applicationContext)
-    val bigPictureStyle = NotificationCompat.BigPictureStyle()
-        .bigPicture(eggImage)
-        .bigLargeIcon(null)
 
-
-    // TODO: Step 2.2 add snooze action
-
-    // TODO: Step 1.2 get an instance of NotificationCompat.Builder
     // Build the notification
     val builder = NotificationCompat.Builder(
         applicationContext,
         applicationContext.getString(R.string.egg_notification_channel_id)
     )
-
-    // TODO: Step 1.8 use the new 'breakfast' notification channel
-
-    builder
         // TODO: Step 1.3 set title, text and icon to builder
         .setSmallIcon(R.drawable.cooked_egg)
         .setContentTitle(applicationContext.getString(R.string.notification_title))
         .setContentText(messageBody)
 
         // TODO: Step 1.13 set content intent
-        .setContentIntent(contentPendingIntent)
+        .setContentIntent(contentPendingIntent(applicationContext))
         .setAutoCancel(true)
 
         // TODO: Step 2.1 add style to builder
-        .setStyle(bigPictureStyle)
+        .setStyle(bigPictureStyle(eggImage))
         .setLargeIcon(eggImage)
 
-
-    // TODO: Step 2.3 add snooze action
+        // TODO: Step 2.3 add snooze action
+        .addAction(
+            R.drawable.egg_icon,
+            applicationContext.getString(R.string.snooze),
+            snoozePendingIntent(applicationContext)
+        )
 
     // TODO: Step 2.5 set priority
 
-    // TODO: Step 1.4 call notify
+
     notify(NOTIFICATION_ID, builder.build())
 }
 
-private fun getEggImage(applicationContext: Context) =
-    BitmapFactory.decodeResource(
-        applicationContext.resources,
-        R.drawable.cooked_egg
+@SuppressLint("UnspecifiedImmutableFlag")
+fun snoozePendingIntent(applicationContext: Context): PendingIntent {
+    val snoozeIntent = Intent(applicationContext, SnoozeReceiver::class.java)
+    return PendingIntent.getBroadcast(
+        applicationContext,
+        REQUEST_CODE,
+        snoozeIntent,
+        PendingIntent.FLAG_ONE_SHOT
     )
+}
+
+@SuppressLint("UnspecifiedImmutableFlag")
+fun contentPendingIntent(applicationContext: Context): PendingIntent {
+    val contentIntent = Intent(applicationContext, MainActivity::class.java)
+    return PendingIntent.getActivity(
+        applicationContext,
+        NOTIFICATION_ID,
+        contentIntent,
+        PendingIntent.FLAG_UPDATE_CURRENT
+    )
+}
+
+private fun bigPictureStyle(image: Bitmap?) =
+    NotificationCompat.BigPictureStyle()
+        .bigPicture(image)
+        .bigLargeIcon(null)
+
+private fun getEggImage(applicationContext: Context) =
+    BitmapFactory.decodeResource(applicationContext.resources, R.drawable.cooked_egg)
